74ede6d31de3ef43417f07c2a94863c20c499f95
Chad Lewis <chadlewis53@gmail.com>
2020-01-10 10:42:25 -0800

    Initial commit

---
+++ src/chatbot.h
@@ -0,0 +1,60 @@
+#ifndef CHATBOT_H_
+#define CHATBOT_H_
+
+#include <wx/bitmap.h>
+#include <string>
+
+class GraphNode; // forward declaration
+class ChatLogic; // forward declaration
+
+class ChatBot
+{
+private:
+    // data handles (owned)
+    wxBitmap *_image; // avatar image
+
+    // data handles (not owned)
+    GraphNode *_currentNode;
+    GraphNode *_rootNode;
+    ChatLogic *_chatLogic;
+
+    // proprietary functions
+    int ComputeLevenshteinDistance(std::string s1, std::string s2);
+
+public:
+    // constructors / destructors
+    ChatBot();                     // constructor WITHOUT memory allocation
+    ChatBot(std::string filename); // constructor WITH memory allocation
+    
+    // Rule of Five #1 - Destructor:
+    ~ChatBot();
+
+    //// STUDENT CODE
+    ////
+
+    // Rule of Five #2 - Copy Constructor:
+    ChatBot(const ChatBot &source);
+
+    // Rule of Five #3 - Copy Assignment Operator:
+    ChatBot &operator=(const ChatBot &source);
+
+    // Rule of Five #4 - Move Constructor:
+    ChatBot(ChatBot &&source);
+
+    // Rule of Five #5 - Move Assignment Operator:
+    ChatBot &operator=(ChatBot &&source);
+
+    ////
+    //// EOF STUDENT CODE
+
+    // getters / setters
+    void SetCurrentNode(GraphNode *node);
+    void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
+    void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
+    wxBitmap *GetImageHandle() { return _image; }
+
+    // communication
+    void ReceiveMessageFromUser(std::string message);
+};
+
+#endif /* CHATBOT_H_ */
---
+++ src/graphnode.cpp
@@ -0,0 +1,67 @@
+// TODO - REMOVE THIS ******* ////
+#include <iostream>
+
+#include "graphedge.h"
+#include "graphnode.h"
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+
+    std::cout << "Something from ~GraphNode() destructor... from line 14 graphnode.cpp" << std::endl;
+
+    delete _chatBot; 
+
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+{
+    _childEdges.push_back(edge);
+}
+
+//// STUDENT CODE
+////
+void GraphNode::MoveChatbotHere(ChatBot *chatbot)
+{
+    std::cout << "Something from MoveChatbotHere()...from line 41, graphnode.cpp" << std::endl;
+    _chatBot = chatbot;
+    _chatBot->SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(_chatBot);
+    _chatBot = nullptr; // invalidate pointer at source
+}
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+    std::cout << "Something from GetChildEdgeAtIndex...from line 58, graphnode.cpp" << std::endl;
+
+    return _childEdges[index];
+
+    ////
+    //// EOF STUDENT CODE
+}
